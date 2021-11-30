1133408aff6ed2d152e44c8378c9e4902e512bc7
MegaArasan <megaarasan2000@gmail.com>
2021-11-30 16:09:57 +0530

    organized

---
+++ CreateMovie.js
@@ -0,0 +1,21 @@
+import { client } from "./index.js";
+
+async function CreateMovie(data) {
+  return await client.db("demo").collection("movies").insertMany(data);
+}
+async function FilteringMovies(filter) {
+  return await client.db("demo").collection("movies").find(filter).toArray();
+}
+async function FindMovie(id) {
+  return await client.db("demo").collection("movies").findOne({ id: id });
+}
+async function DeleteMovie(id) {
+  return await client.db("demo").collection("movies").deleteOne({ id: id });
+}
+async function UpdateMovie(id, data) {
+  return await client
+    .db("demo")
+    .collection("movies")
+    .updateOne({ id: id }, { $set: data });
+}
+export { FilteringMovies, CreateMovie, FindMovie, DeleteMovie, UpdateMovie };
