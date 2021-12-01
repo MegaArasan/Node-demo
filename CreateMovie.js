d2069ef18a8375ca34ca9d7c51fe73832fe3a102
MegaArasan <megaarasan2000@gmail.com>
2021-12-01 23:17:35 +0530

    dblegitid

--- CreateMovie.js
+++ CreateMovie.js
@@ -1,5 +1,5 @@
+import { ObjectId } from "mongodb";
 import { client } from "./index.js";
-
 
 async function CreateMovie(data) {
   return await client.db("demo").collection("movies").insertMany(data);
@@ -8,15 +8,22 @@ async function FilteringMovies(filter) {
   return await client.db("demo").collection("movies").find(filter).toArray();
 }
 async function FindMovie(id) {
-  return await client.db("demo").collection("movies").findOne({ id: id });
+  console.log(id);
+  return await client
+    .db("demo")
+    .collection("movies")
+    .findOne({ _id: ObjectId(id) });
 }
 async function DeleteMovie(id) {
-  return await client.db("demo").collection("movies").deleteOne({ id: id });
+  return await client
+    .db("demo")
+    .collection("movies")
+    .deleteOne({ _id: ObjectId(id) });
 }
 async function UpdateMovie(id, data) {
   return await client
     .db("demo")
     .collection("movies")
-    .updateOne({ id: id }, { $set: data });
+    .updateOne({ _id: ObjectId(id) }, { $set: data });
 }
 export { FilteringMovies, CreateMovie, FindMovie, DeleteMovie, UpdateMovie };
